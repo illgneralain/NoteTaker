fa06f49d4fcc9c5f8c87e114f89f03578bd0cb45
Illgner Alain <illgneralain@gmail.com>
2021-01-30 08:18:22 -0500

    finished adding express set up and moving on to routes

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const app = express();
 
-cont PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
