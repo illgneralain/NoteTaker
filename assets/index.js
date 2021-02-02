828a9bcfc371529b4d4ce9eda39197ace0cac890
Illgner Alain <illgneralain@gmail.com>
2021-02-02 11:26:19 -0500

    all js functions for deleting saving and retrieving notes

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,32 @@
+const { notEqual } = require("assert");
+
 let $noteTitle = $(".note-title");
 let $saveNoteBtn = $(".save-note");
 let $noteText = $(".note-textarea");
 let $newNoteBtn = $(".new=note");
 let $noteList = $(".list-container .list-group"); 
+
+let activeNote = {};
+
+const getNotes = () => {
+    return $.ajax({
+        url: "/",
+        method: "GET",
+    });
+};
+
+const saveNote = (note) => {
+    return $.ajax({
+        url: "/",
+        data: note,
+        method: "POST",
+    });
+};
+
+const deleteNote = (id) => {
+    return $.ajax({
+        url: "/",
+        data: note,
+        method: "GET",
+    });
+};
