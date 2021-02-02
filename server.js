11bd19fb63b191d56734c4fd5aaca1ad52c756ff
Illgner Alain <illgneralain@gmail.com>
2021-02-02 08:51:13 -0500

    saving changes that was made a few days ago, happy i didnt lose it

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require("express");
+var express = require("express");
 
-const app = express();
+var app = express();
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
+
 
 require('./routes')
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
