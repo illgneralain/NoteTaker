4164701bd8a2133755a5ebad7009b865c9164c07
Illgner Alain <illgneralain@gmail.com>
2021-02-02 11:21:29 -0500

    added the functionality to have our note list be contained within the list group within our list container created

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,5 @@
 let $noteTitle = $(".note-title");
 let $saveNoteBtn = $(".save-note");
+let $noteText = $(".note-textarea");
+let $newNoteBtn = $(".new=note");
+let $noteList = $(".list-container .list-group"); 
