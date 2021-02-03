0f14273d7a36247f6c846d4e1778f26a2ced63a6
Illgner Alain <illgneralain@gmail.com>
2021-02-03 00:10:01 -0500

    adding final stuff

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+import Express from "express";
+
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3006;
+ 
 
-
-// let notesData = [];
+// let notesData = []; 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
